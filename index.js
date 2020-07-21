0eb41dfb5769ed6907ad10b1fe00bd60371637dc
Pablo Vargas <juan.p.vargas@gsk.com>
2020-07-21 12:39:48 -0600

    Reverted that change lol

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.get('/', function (req, res) {
     res.send('Hello World!');
   });
-app.listen(server_port, "0.0.0.0", function () {
+app.listen(server_port, server_ip_address, function () {
 console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
