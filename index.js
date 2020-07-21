337641c4201262fe8a54beb90968a03d4e825885
Pablo Vargas <juan.p.vargas@gsk.com>
2020-07-21 12:20:30 -0600

    Added the express app and modified the dependencies in the package file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, World")
+var express = require('express');
+var app = express();
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+app.listen(server_port, server_ip_address, function () {
+console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
